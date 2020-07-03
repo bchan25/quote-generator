7ff85fedad7a4f3783732aad17ab3b6d1d126f19
Ben Chan <ben@chanhq.com>
2020-07-03 15:26:14 +0100

    Fetch quote implemented with CORS error

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-// Used to debug code to console
-console.log("testing");
+// Get Quote From API
+// Async funtion
+async function getQuote() {
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang-en&format=json";
+
+  try {
+    // Wait to fill in const before moving on
+    const response = await fetch(apiUrl);
+    const data = await response.json;
+    console.log(data);
+  } catch (error) {
+    console.log("whoops, no quote", error);
+  }
+}
+
+// On Load
+getQuote();
