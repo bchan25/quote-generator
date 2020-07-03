ce6b5f7df86843feecfe373e59a441824b3a7909
Ben Chan <ben@chanhq.com>
2020-07-03 17:03:19 +0100

    Fixed proxy server

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function removeLoadingSpinner() {
 async function getQuote() {
   showLoadingSpinner();
   // Proxy to fixed CORS error
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const proxyUrl = "https://warm-citadel-96082.herokuapp.com";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
