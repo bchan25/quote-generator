c70bc864c0278c7e957e861377514210faa3569f
Ben Chan <ben@chanhq.com>
2020-07-03 15:38:40 +0100

    Added a proxy solution to CORS issue

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // Get Quote From API
 // Async funtion
 async function getQuote() {
+  // Proxy to fixed CORS error
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const apiUrl =
-    "http://api.forismatic.com/api/1.0/?method=getQuote&lang-en&format=json";
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
   try {
     // Wait to fill in const before moving on
-    const response = await fetch(apiUrl);
+    const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json;
     console.log(data);
   } catch (error) {
+    getQuote();
     console.log("whoops, no quote", error);
   }
 }
